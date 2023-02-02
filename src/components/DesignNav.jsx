b2934e4a43961466feb4bd35b5085985cf5b7845
Kouhei Ho <alan829221@gmail.com>
2023-02-02 14:44:19 +0800

    UPDATE: SubNav Sellect Effect

--- src/components/DesignNav.jsx
+++ src/components/DesignNav.jsx
@@ -2,10 +2,12 @@
 
 import DesignContext from "../context/DesignContext";
 import { useContext } from "react";
+import { useState } from "react";
 
 function DesignNav() {
   const { works, worksSlicePages, setShowWorks, setCurrentPage } =
     useContext(DesignContext);
+  const [tag, setTag] = useState("all");
 
   const filterTag = (tag) => {
     if (tag === "all") {
@@ -24,28 +26,70 @@ function DesignNav() {
     <div className="h-20 flex justify-start items-end px-12 bg-primary-yellow text-primary-black">
       <div>
         <button
-          className="m-4 hover:cursor-pointer"
-          onClick={() => filterTag("all")}
+          className={
+            tag === "all"
+              ? "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-secondary-black before:bottom-0 before:left-1/2 before:h-full before:w-full before:-translate-x-1/2 before:rounded-t"
+              : "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-black before:bottom-1 before:left-1/2 before:h-1 before:w-full  before:-translate-x-1/2 before:scale-[0] before:opacity-0 hover:before:scale-100 hover:before:opacity-100 before:transition before:ease-in-out before:duration-500"
+          }
+          onClick={() => {
+            filterTag("all");
+            setTag("all");
+          }}
         >
-          All
+          <span className={tag === "all" ? "relative text-primary-yellow" : ""}>
+            All
+          </span>
         </button>
         <button
-          className="m-4 hover:cursor-pointer"
-          onClick={() => filterTag("brand")}
+          className={
+            tag === "brand"
+              ? "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-secondary-black before:bottom-0 before:left-1/2 before:h-full before:w-full before:-translate-x-1/2 before:rounded-t"
+              : "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-black before:bottom-1 before:left-1/2 before:h-1 before:w-full  before:-translate-x-1/2 before:scale-[0] before:opacity-0 hover:before:scale-100 hover:before:opacity-100 before:transition before:ease-in-out before:duration-500"
+          }
+          onClick={() => {
+            filterTag("brand");
+            setTag("brand");
+          }}
         >
-          Brand
+          <span
+            className={tag === "brand" ? "relative text-primary-yellow" : ""}
+          >
+            Brand
+          </span>
         </button>
         <button
-          className="m-4 hover:cursor-pointer"
-          onClick={() => filterTag("package")}
+          className={
+            tag === "package"
+              ? "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-secondary-black before:bottom-0 before:left-1/2 before:h-full before:w-full before:-translate-x-1/2 before:rounded-t"
+              : "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-black before:bottom-1 before:left-1/2 before:h-1 before:w-full  before:-translate-x-1/2 before:scale-[0] before:opacity-0 hover:before:scale-100 hover:before:opacity-100 before:transition before:ease-in-out before:duration-500"
+          }
+          onClick={() => {
+            filterTag("package");
+            setTag("package");
+          }}
         >
-          Package
+          <span
+            className={tag === "package" ? "relative text-primary-yellow" : ""}
+          >
+            Package
+          </span>
         </button>
         <button
-          className="m-4 hover:cursor-pointer"
-          onClick={() => filterTag("others")}
+          className={
+            tag === "others"
+              ? "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-secondary-black before:bottom-0 before:left-1/2 before:h-full before:w-full before:-translate-x-1/2 before:rounded-t"
+              : "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-black before:bottom-1 before:left-1/2 before:h-1 before:w-full  before:-translate-x-1/2 before:scale-[0] before:opacity-0 hover:before:scale-100 hover:before:opacity-100 before:transition before:ease-in-out before:duration-500"
+          }
+          onClick={() => {
+            filterTag("others");
+            setTag("others");
+          }}
         >
-          Others
+          <span
+            className={tag === "others" ? "relative text-primary-yellow" : ""}
+          >
+            Others
+          </span>
         </button>
       </div>
     </div>
--- src/components/WebNav.jsx
+++ src/components/WebNav.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import WebContext from "../context/WebContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function WebNav() {
   const { works, worksSlicePages, setShowWorks } = useContext(WebContext);
+  const [tag, setTag] = useState("all");
 
   const filterTag = (tag) => {
     if (tag === "all") {
@@ -22,22 +23,51 @@ function WebNav() {
       <div className="h-20 flex justify-start items-end px-12 bg-primary-blue text-primary-black">
         <div>
           <button
-            className="m-4 hover:cursor-pointer"
-            onClick={() => filterTag("all")}
+            className={
+              tag === "all"
+                ? "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-secondary-black before:bottom-0 before:left-1/2 before:h-full before:w-full before:-translate-x-1/2 before:rounded-t"
+                : "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-black before:bottom-1 before:left-1/2 before:h-1 before:w-full  before:-translate-x-1/2 before:scale-[0] before:opacity-0 hover:before:scale-100 hover:before:opacity-100 before:transition before:ease-in-out before:duration-500"
+            }
+            onClick={() => {
+              filterTag("all");
+              setTag("all");
+            }}
           >
-            All
+            <span className={tag === "all" ? "relative text-primary-blue" : ""}>
+              All
+            </span>
           </button>
           <button
-            className="m-4 hover:cursor-pointer"
-            onClick={() => filterTag("frontend")}
+            className={
+              tag === "frontend"
+                ? "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-secondary-black before:bottom-0 before:left-1/2 before:h-full before:w-full before:-translate-x-1/2 before:rounded-t"
+                : "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-black before:bottom-1 before:left-1/2 before:h-1 before:w-full  before:-translate-x-1/2 before:scale-[0] before:opacity-0 hover:before:scale-100 hover:before:opacity-100 before:transition before:ease-in-out before:duration-500"
+            }
+            onClick={() => {
+              filterTag("frontend");
+              setTag("frontend");
+            }}
           >
-            Fron End
+            <span
+              className={tag === "frontend" ? "relative text-primary-blue" : ""}
+            >
+              Fron End
+            </span>
           </button>
           <button
-            className="m-4 hover:cursor-pointer"
-            onClick={() => filterTag("ui")}
+            className={
+              tag === "ui"
+                ? "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-secondary-black before:bottom-0 before:left-1/2 before:h-full before:w-full before:-translate-x-1/2 before:rounded-t"
+                : "py-2 px-6 hover:cursor-pointer relative before:absolute before:bg-black before:bottom-1 before:left-1/2 before:h-1 before:w-full  before:-translate-x-1/2 before:scale-[0] before:opacity-0 hover:before:scale-100 hover:before:opacity-100 before:transition before:ease-in-out before:duration-500"
+            }
+            onClick={() => {
+              filterTag("ui");
+              setTag("ui");
+            }}
           >
-            UI / UX
+            <span className={tag === "ui" ? "relative text-primary-blue" : ""}>
+              UI / UX
+            </span>
           </button>
         </div>
       </div>
