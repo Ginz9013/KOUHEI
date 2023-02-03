d2219d8f908ae8032d80599c4a26cd38f2088331
Kouhei Ho <alan829221@gmail.com>
2023-02-03 12:18:47 +0800

    UPDATE: WebWorkPage Effect

--- src/components/WebWorkLists.jsx
+++ src/components/WebWorkLists.jsx
@@ -2,17 +2,35 @@
 import WebWorkCard from "./WebWorkCard";
 
 import WebContext from "../context/WebContext";
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 function WebWorkLists() {
   const { showWorks, currentPage } = useContext(WebContext);
+  const list = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      // Show Page
+      gsap.from(".gsap-current", {
+        opacity: 0,
+        duration: 0.5,
+        ease: "power2.inOut",
+      });
+    }, list);
+
+    return () => ctx.revert();
+  }, [showWorks, currentPage]);
 
   return (
-    <div className="grow flex justify-center items-center text-white">
+    <div
+      ref={list}
+      className="grow flex justify-center items-center text-white"
+    >
       {showWorks.map((page, index) => (
         <div
           key={index}
-          className={index === currentPage - 1 ? "block" : "hidden"}
+          className={index === currentPage - 1 ? "gsap-current" : "hidden"}
         >
           <ul className="grid grid-cols-2 grid-rows-3 gap-6">
             {page.map((work) => (
